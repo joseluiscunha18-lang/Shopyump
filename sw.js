9d409e2d9042389100b5dbb93d3306ceb8396890
joseluiscunha18-lang <joseluiscunha18@gmail.com>
2026-06-18 21:03:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'shopyump-pwa-v1';
+const urlsToCache = [
+  '/Pwa.html',
+  '/manifest.json',
+  '/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
